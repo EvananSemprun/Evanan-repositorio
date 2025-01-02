b8a5a5dcb2083449c7530cef051596de88d07298
EvananSemprun <evanansemprun@gmail.com>
2025-01-02 16:51:12 -0400

    raplace cared

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
 import { IconPhoto, IconMessageCircle } from '@tabler/icons-react';
-import { Card, Image, Text, Group, Title, Tabs } from '@mantine/core';
+import { Card, Image, Text, Group, Title, Tabs, Grid } from '@mantine/core';
 
 function App() {
   return (
-    
+
     <div style={{ margin: '0 auto', maxWidth: '90%', width: 800 }}>
       <Card
         shadow="sm"
@@ -48,43 +48,104 @@ function App() {
           >
             <Tabs.List>
               <Tabs.Tab value="gallery" icon={<IconPhoto size={14} />}>
-                Sobre mi
+                Presentacion
               </Tabs.Tab>
-              <Tabs.Tab value="messages" icon={<IconMessageCircle size={14} />}>
+              {/* <Tabs.Tab value="messages" icon={<IconMessageCircle size={14} />}>
                 Datos relevantes
-              </Tabs.Tab>
+              </Tabs.Tab> */}
               <Tabs.Tab value="yes" icon={<IconMessageCircle size={14} />}>
-                Informacion de contacto
+                Sobre mi
               </Tabs.Tab>
             </Tabs.List>
 
             <Tabs.Panel value="gallery" pl="xs">
-              <Title order={2}>Evanan Semprun</Title>
+              <Group mt={5} mb={5} position='apart'>
+
+                <Title order={2}>Evanan Semprun</Title>
+                <Title c='gray' order={6}>Programador Frontend</Title>
+              </Group>
               <Text
-                fz="md"
+                fz="lg"
                 style={{
                   textAlign: 'justify',
                 }}
               >
-                Ingeniero en Computación con sólidos conocimientos en desarrollo de
-                software, sistemas informáticos, redes y seguridad informática.
-                Experiencia en el diseño, implementación y mantenimiento de soluciones
-                tecnológicas. Habilidad para trabajar en equipo, resolver problemas de
-                manera eficiente y adaptarse a entornos cambiantes.
+                <span style={{ color: "red" }}>2 </span>
+                Años de experiencia como Programador Frontend en Venezuela.
+                Especializado en la resolución de bugs y creación de páginas web totalmente responsivas.
               </Text>
+
             </Tabs.Panel>
 
             <Tabs.Panel value="messages" pl="xs">
-              <Text fz="md" style={{ textAlign: 'center' }}>
-                ¡Bienvenido a la sección de mensajes! Aquí puedes ver tus comunicaciones
-                recientes.
-              </Text>
+
+
+              <Title order={2}>
+                <span style={{ color: "red" }}>2</span> Años de experiencia
+              </Title>
+              <Grid>
+                <Grid.Col span={4}>
+                  <Card
+                    shadow="sm"
+                    p="lg"
+                    radius="md"
+                    withBorder
+                    style={{
+                      margin: '1rem auto',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      maxWidth: '100%',
+                      width: '100%',
+                    }}
+                  >
+                    s
+                  </Card>
+
+                </Grid.Col>
+
+                {/* <Grid.Col span={4}>
+                  <Card
+                    shadow="sm"
+                    p="lg"
+                    radius="md"
+                    withBorder
+                    style={{
+                      margin: '1rem auto',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      maxWidth: '100%',
+                      width: '100%',
+                    }}
+                  >
+                    s
+                  </Card>
+                </Grid.Col> */}
+
+                <Grid.Col span={4}>
+                  <Card
+                    shadow="sm"
+                    p="lg"
+                    radius="md"
+                    withBorder
+                    style={{
+                      margin: '1rem auto',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      maxWidth: '100%',
+                      width: '100%',
+                    }}
+                  >
+                    10 proyectos participados
+                  </Card>
+                </Grid.Col>
+              </Grid>
             </Tabs.Panel>
 
             <Tabs.Panel value="yes" pl="xs">
               <Text fz="md" style={{ textAlign: 'center' }}>
-                ¡Bienvenido a la sección de mensajes! Aquí puedes ver tus comunicaciones
-                recientes.
+                Ingeniero en Computación con sólidos conocimientos en desarrollo de software y sistemas informáticos.
+                Interesado en redes y seguridad informática, con experiencia en el diseño, implementación y mantenimiento de soluciones tecnológicas. 
+                Destacado por la habilidad para trabajar en equipo, resolver problemas de manera eficiente y adaptarse a entornos dinámicos y cambiantes.
               </Text>
             </Tabs.Panel>
 
