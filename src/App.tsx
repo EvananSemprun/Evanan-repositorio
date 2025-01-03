f56e6c3c4d5f17eecea7231ebbe3a4ed1b758dd7
EvananSemprun <evanansemprun@gmail.com>
2025-01-03 10:47:36 -0400

    skill

--- src/App.tsx
+++ src/App.tsx
@@ -1,158 +1,141 @@
 import './App.css';
+import Skill from './Components/Skill';
 import { IconPhoto, IconMessageCircle } from '@tabler/icons-react';
 import { Card, Image, Text, Group, Title, Tabs, Grid } from '@mantine/core';
 
 function App() {
   return (
-
-    <div style={{ margin: '0 auto', maxWidth: '90%', width: 800 }}>
-      <Card
-        shadow="sm"
-        p="lg"
-        radius="md"
-        withBorder
-        style={{
-          margin: '1rem auto',
-          display: 'flex',
-          flexDirection: 'column',
-          maxWidth: '100%',
-          width: '100%',
-        }}
-      >
-        <Group
-          align="stretch"
-          spacing="xl"
+    <>
+      <div style={{ margin: '0 auto', maxWidth: '90%', width: 800 }}>
+        <Card
+          shadow="sm"
+          p="lg"
+          radius="md"
+          withBorder
           style={{
+            margin: '1rem auto',
             display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            flexWrap: 'nowrap',
+            flexDirection: 'column',
+            maxWidth: '100%',
             width: '100%',
           }}
         >
-          <div style={{ flexShrink: 0, width: '35%', height: '100%' }}>
-            <Image
-              radius="md"
-              src="/eva.jpg"
-              alt="Imagen aleatoria"
-              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-            />
-          </div>
-
-          <Tabs
-            color="yellow"
-            radius="xs"
-            orientation="horizontal"
-            defaultValue="gallery"
-            style={{ flex: 1, paddingLeft: '1rem' }}
+          <Group
+            align="stretch"
+            spacing="xl"
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              flexWrap: 'nowrap',
+              width: '100%',
+            }}
           >
-            <Tabs.List>
-              <Tabs.Tab value="gallery" icon={<IconPhoto size={14} />}>
-                Presentacion
-              </Tabs.Tab>
-              {/* <Tabs.Tab value="messages" icon={<IconMessageCircle size={14} />}>
-                Datos relevantes
-              </Tabs.Tab> */}
-              <Tabs.Tab value="yes" icon={<IconMessageCircle size={14} />}>
-                Sobre mi
-              </Tabs.Tab>
-            </Tabs.List>
+            <div style={{ flexShrink: 0, width: '35%', height: '100%' }}>
+              <Image
+                radius="md"
+                src="/eva.jpg"
+                alt="Imagen aleatoria"
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              />
+            </div>
 
-            <Tabs.Panel value="gallery" pl="xs">
-              <Group mt={5} mb={5} position='apart'>
+            <Tabs
+              color="yellow"
+              radius="xs"
+              orientation="horizontal"
+              defaultValue="gallery"
+              style={{ flex: 1, paddingLeft: '1rem' }}
+            >
+              <Tabs.List>
+                <Tabs.Tab value="gallery" icon={<IconPhoto size={14} />}>
+                  Presentacion
+                </Tabs.Tab>
 
-                <Title order={2}>Evanan Semprun</Title>
-                <Title c='gray' order={6}>Programador Frontend</Title>
-              </Group>
-              <Text
-                fz="lg"
-                style={{
-                  textAlign: 'justify',
-                }}
-              >
-                <span style={{ color: "red" }}>2 </span>
-                Años de experiencia como Programador Frontend en Venezuela.
-                Especializado en la resolución de bugs y creación de páginas web totalmente responsivas.
-              </Text>
+                <Tabs.Tab value="yes" icon={<IconMessageCircle size={14} />}>
+                  Sobre mi
+                </Tabs.Tab>
+              </Tabs.List>
 
-            </Tabs.Panel>
+              <Tabs.Panel value="gallery" pl="xs">
+                <Group mt={5} mb={5} position='apart'>
 
-            <Tabs.Panel value="messages" pl="xs">
+                  <Title order={2}>Evanan Semprun</Title>
+                  <Title c='gray' order={6}>Programador Frontend</Title>
+                </Group>
+                <Text
+                  fz="lg"
+                  style={{
+                    textAlign: 'justify',
+                  }}
+                >
+                  <span style={{ color: "red" }}>2 </span>
+                  Años de experiencia como Programador Frontend en Venezuela.
+                  Especializado en la resolución de bugs y creación de páginas web totalmente responsivas.
+                </Text>
 
+              </Tabs.Panel>
 
-              <Title order={2}>
-                <span style={{ color: "red" }}>2</span> Años de experiencia
-              </Title>
-              <Grid>
-                <Grid.Col span={4}>
-                  <Card
-                    shadow="sm"
-                    p="lg"
-                    radius="md"
-                    withBorder
-                    style={{
-                      margin: '1rem auto',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      maxWidth: '100%',
-                      width: '100%',
-                    }}
-                  >
-                    s
-                  </Card>
+              <Tabs.Panel value="messages" pl="xs">
 
-                </Grid.Col>
+                <Title order={2}>
+                  <span style={{ color: "red" }}>2</span> Años de experiencia
+                </Title>
+                <Grid>
+                  <Grid.Col span={4}>
+                    <Card
+                      shadow="sm"
+                      p="lg"
+                      radius="md"
+                      withBorder
+                      style={{
+                        margin: '1rem auto',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        maxWidth: '100%',
+                        width: '100%',
+                      }}
+                    >
+                      s
+                    </Card>
 
-                {/* <Grid.Col span={4}>
-                  <Card
-                    shadow="sm"
-                    p="lg"
-                    radius="md"
-                    withBorder
-                    style={{
-                      margin: '1rem auto',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      maxWidth: '100%',
-                      width: '100%',
-                    }}
-                  >
-                    s
-                  </Card>
-                </Grid.Col> */}
+                  </Grid.Col>
 
-                <Grid.Col span={4}>
-                  <Card
-                    shadow="sm"
-                    p="lg"
-                    radius="md"
-                    withBorder
-                    style={{
-                      margin: '1rem auto',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      maxWidth: '100%',
-                      width: '100%',
-                    }}
-                  >
-                    10 proyectos participados
-                  </Card>
-                </Grid.Col>
-              </Grid>
-            </Tabs.Panel>
+                  <Grid.Col span={4}>
+                    <Card
+                      shadow="sm"
+                      p="lg"
+                      radius="md"
+                      withBorder
+                      style={{
+                        margin: '1rem auto',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        maxWidth: '100%',
+                        width: '100%',
+                      }}
+                    >
+                      10 proyectos participados
+                    </Card>
+                  </Grid.Col>
+                </Grid>
+              </Tabs.Panel>
 
-            <Tabs.Panel value="yes" pl="xs">
-              <Text fz="md" style={{ textAlign: 'center' }}>
-                Ingeniero en Computación con sólidos conocimientos en desarrollo de software y sistemas informáticos.
-                Interesado en redes y seguridad informática, con experiencia en el diseño, implementación y mantenimiento de soluciones tecnológicas. 
-                Destacado por la habilidad para trabajar en equipo, resolver problemas de manera eficiente y adaptarse a entornos dinámicos y cambiantes.
-              </Text>
-            </Tabs.Panel>
+              <Tabs.Panel value="yes" pl="xs">
+                <Text fz="md" style={{ textAlign: 'center' }}>
+                  Ingeniero en Computación con sólidos conocimientos en desarrollo de software y sistemas informáticos.
+                  Interesado en redes y seguridad informática, con experiencia en el diseño, implementación y mantenimiento de soluciones tecnológicas.
+                  Destacado por la habilidad para trabajar en equipo, resolver problemas de manera eficiente y adaptarse a entornos dinámicos y cambiantes.
+                </Text>
+              </Tabs.Panel>
 
-          </Tabs>
-        </Group>
-      </Card>
-    </div>
+            </Tabs>
+          </Group>
+        </Card>
+      </div>
+
+      <Skill />
+    </>
   );
 }
 
---
+++ src/Components/Skill.tsx
@@ -0,0 +1,325 @@
+import { Grid, Card, Group, Image } from '@mantine/core';
+
+function Skill() {
+  return (
+    <>
+      <Group
+        spacing="lg"
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+          alignItems: 'flex-start',
+        }}
+      >
+        <div style={{ flex: '1 1 60%', maxWidth: '60%' }}>
+
+          <Grid>
+            <Grid.Col md={6} lg={2.4}>
+              <Card
+                shadow="sm"
+                p="lg"
+                radius="md"
+                withBorder
+                style={{
+                  margin: '1rem auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  maxWidth: '100%',
+                  width: '100%',
+                }}
+              >
+                <div style={{ width: '100%', margin: 'auto' }}>
+                  <Image
+                    radius="md"
+                    src="/html.png"
+                    alt="Random unsplash image"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      objectFit: 'cover',
+                    }}
+                  />
+                </div>
+              </Card>
+            </Grid.Col>
+            <Grid.Col md={6} lg={2.4}>
+              <Card
+                shadow="sm"
+                p="lg"
+                radius="md"
+                withBorder
+                style={{
+                  margin: '1rem auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  maxWidth: '100%',
+                  width: '100%',
+                }}
+              >
+                <div style={{ width: '100%', margin: 'auto' }}>
+                  <Image
+                    radius="md"
+                    src="/js.png"
+                    alt="Random unsplash image"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      objectFit: 'cover',
+                    }}
+                  />
+                </div>
+              </Card>
+            </Grid.Col>
+            <Grid.Col md={6} lg={2.4}>
+              <Card
+                shadow="sm"
+                p="lg"
+                radius="md"
+                withBorder
+                style={{
+                  margin: '1rem auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  maxWidth: '100%',
+                  width: '100%',
+                }}
+              >
+                <div style={{ width: '100%', margin: 'auto' }}>
+                  <Image
+                    radius="md"
+                    src="/react.png"
+                    alt="Random unsplash image"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      objectFit: 'cover',
+                    }}
+                  />
+                </div>
+              </Card>
+            </Grid.Col>
+            <Grid.Col md={6} lg={2.4}>
+              <Card
+                shadow="sm"
+                p="lg"
+                radius="md"
+                withBorder
+                style={{
+                  margin: '1rem auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  maxWidth: '100%',
+                  width: '100%',
+                }}
+              >
+                <div style={{ width: '100%', margin: 'auto' }}>
+                  <Image
+                    radius="md"
+                    src="/ts.png"
+                    alt="Random unsplash image"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      objectFit: 'cover',
+                    }}
+                  />
+                </div>
+              </Card>
+            </Grid.Col>
+            <Grid.Col md={6} lg={2.4}>
+              <Card
+                shadow="sm"
+                p="lg"
+                radius="md"
+                withBorder
+                style={{
+                  margin: '1rem auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  maxWidth: '100%',
+                  width: '100%',
+                }}
+              >
+                <div style={{ width: '100%', margin: 'auto' }}>
+                  <Image
+                    radius="md"
+                    src="/c.png"
+                    alt="Random unsplash image"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      objectFit: 'cover',
+                    }}
+                  />
+                </div>
+              </Card>
+            </Grid.Col>
+          </Grid>
+
+          <Grid>
+            <Grid.Col md={6} lg={2.4}>
+              <Card
+                shadow="sm"
+                p="lg"
+                radius="md"
+                withBorder
+                style={{
+                  margin: '1rem auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  maxWidth: '100%',
+                  width: '100%',
+                }}
+              >
+                <div style={{ width: '100%', margin: 'auto' }}>
+                  <Image
+                    radius="md"
+                    src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                    alt="Random unsplash image"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      objectFit: 'cover',
+                    }}
+                  />
+                </div>
+              </Card>
+            </Grid.Col>
+            <Grid.Col md={6} lg={2.4}>
+              <Card
+                shadow="sm"
+                p="lg"
+                radius="md"
+                withBorder
+                style={{
+                  margin: '1rem auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  maxWidth: '100%',
+                  width: '100%',
+                }}
+              >
+                <div style={{ width: '100%', margin: 'auto' }}>
+                  <Image
+                    radius="md"
+                    src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                    alt="Random unsplash image"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      objectFit: 'cover',
+                    }}
+                  />
+                </div>
+              </Card>
+            </Grid.Col>
+            <Grid.Col md={6} lg={2.4}>
+              <Card
+                shadow="sm"
+                p="lg"
+                radius="md"
+                withBorder
+                style={{
+                  margin: '1rem auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  maxWidth: '100%',
+                  width: '100%',
+                }}
+              >
+                <div style={{ width: '100%', margin: 'auto' }}>
+                  <Image
+                    radius="md"
+                    src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                    alt="Random unsplash image"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      objectFit: 'cover',
+                    }}
+                  />
+                </div>
+              </Card>
+            </Grid.Col>
+            <Grid.Col md={6} lg={2.4}>
+              <Card
+                shadow="sm"
+                p="lg"
+                radius="md"
+                withBorder
+                style={{
+                  margin: '1rem auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  maxWidth: '100%',
+                  width: '100%',
+                }}
+              >
+                <div style={{ width: '100%', margin: 'auto' }}>
+                  <Image
+                    radius="md"
+                    src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                    alt="Random unsplash image"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      objectFit: 'cover',
+                    }}
+                  />
+                </div>
+              </Card>
+            </Grid.Col>
+            <Grid.Col md={6} lg={2.4}>
+              <Card
+                shadow="sm"
+                p="lg"
+                radius="md"
+                withBorder
+                style={{
+                  margin: '1rem auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  maxWidth: '100%',
+                  width: '100%',
+                }}
+              >
+                <div style={{ width: '100%', margin: 'auto' }}>
+                  <Image
+                    radius="md"
+                    src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                    alt="Random unsplash image"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      objectFit: 'cover',
+                    }}
+                  />
+                </div>
+              </Card>
+            </Grid.Col>
+          </Grid>
+        </div>
+
+        <Card
+          shadow="sm"
+          p="lg"
+          radius="md"
+          withBorder
+          style={{
+            flex: '1 1 35%',
+            maxWidth: '35%',
+            margin: '1rem auto',
+          }}
+        >
+          <div>
+            <h3>Información</h3>
+
+          </div>
+        </Card>
+      </Group>
+    </>
+  );
+}
+
+export default Skill;
